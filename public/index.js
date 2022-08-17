1630a89771ee98ab8b1ba6bd662512bcbdf83fa4
daiki_takano <dddaiki.geek@gmail.com>
2022-08-17 09:03:57 +0900

    f

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-app.js';
+// import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.9.1/firebase-analytics.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,11 +9,12 @@
 const firebaseConfig = {
   apiKey: "AIzaSyC2uGQbTynGIm2AzMbGF1CO_A3ieF_5kL4",
   authDomain: "issue-card.firebaseapp.com",
+  databaseURL: "https://issue-card-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "issue-card",
   storageBucket: "issue-card.appspot.com",
   messagingSenderId: "936978701874",
-  appId: "1:936978701874:web:d7f2c3d7bb489de6afe62b",
-  measurementId: "G-W5HTB0QYTB"
+  appId: "1:936978701874:web:75b386a426b4327dafe62b",
+  measurementId: "G-8FMTNDN7XC"
 };
 
 // Initialize Firebase
